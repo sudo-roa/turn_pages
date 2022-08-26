b33d760e48cc16317a759b91edb8272a3f640bf4
sudo-roa <sudo.roa9393@gmail.com>
2022-08-26 15:17:04 +0900

    add test

---
+++ test/js/turn_pages.js
@@ -0,0 +1,12 @@
+document.getElementById("r2").addEventListener("click", toggle);
+
+function toggle(){
+    console.log("clicked");
+    var childcontent = this.firstElementChild;
+    var nextcontent = this.nextElementSibling;
+    var nextchildcontent = nextcontent.firstElementChild;
+    this.classList.toggle("is-turned");
+    childcontent.classList.toggle("is-turned");
+    nextcontent.classList.toggle("is-turned");
+    nextchildcontent.classList.toggle("is-turned");
+}
